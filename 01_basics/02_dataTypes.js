3175d67347ac1b8dd8bbdbae061777d5458887ee
PrashantMakwana05 <prashantkumar01.005@gmail.com>
2024-10-01 06:13:11 +0000

    Data types in Javascript

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,16 @@
+let age =19
+let name = "Suraj"
+let isloggedIn = true
+let state = null
+let gender
+
+// number = 2 to power 53
+// bigint
+// string = "" or ''
+// boolean = true/false
+// null = standalone value(empty)
+// undefined = undefined
+// symbol = unique
+
+console.log(typeof gender);
+console.table([typeof age, typeof name, typeof isloggedIn, typeof state, typeof gender]);
