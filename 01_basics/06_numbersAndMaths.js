ec3d843b1a1948d972eb77a1c602584ba926b181
PrashantMakwana05 <prashantkumar01.005@gmail.com>
2024-10-01 06:40:21 +0000

    Numbers and Mathematic Operations

---
+++ 01_basics/06_numbersAndMaths.js
@@ -0,0 +1,30 @@
+const score = 400
+
+const balance = new Number(100)
+// console.log(balance);
+// console.log(balance.toFixed(2));
+// console.log(balance.toString().length);
+
+const otherNum = 2005.957899
+// console.log(otherNum.toPrecision(4));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++>> Maths <<+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math.abs(-10));
+// console.log(Math.round(4.7));
+// console.log(Math.floor(4.7));
+// console.log(Math.ceil(4.7));
+// console.log(Math.min(2,4,5,6,8));
+// console.log(Math.max(2,4,5,6,8));
+
+// console.log((Math.random()*10) + 1);
+// console.log((Math.random()*10) + 1);
+
+const min = 10 
+const max = 20
+
+console.log((Math.random()*(max - min + 1)) + min);
+
