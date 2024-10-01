c81300604aa7ff2906ed0ad9d13b5ca632d6c071
PrashantMakwana05 <prashantkumar01.005@gmail.com>
2024-10-01 06:17:13 +0000

    conversions and Operations in Javascript

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,55 @@
+// let score
+
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33a" => NaN
+// null => 0
+// undefind => NaN
+// boolean => 1/0
+
+let isLoggedIn = 1
+let BoolIsLoggedIn = Boolean(isLoggedIn)
+// console.log(BoolIsLoggedIn);
+
+// 1 => true
+// "" => false
+// "anything" =>true
+
+
+//=====================================================>>>OPERATIONS<<<=============================================================
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+
+let str1 = "Good "
+let str2 = "Morning"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log((3+4) * 5 % 3)
+
+console.log(+true);
+console.log(+"");
+
+let gameCounter = 100
+console.log(gameCounter++);
+console.log(++gameCounter);
