1cd678c0c3f6b4d4f1aec0caebd79cf5f6fb384a
PrashantMakwana05 <prashantkumar01.005@gmail.com>
2024-10-08 08:07:15 +0000

    Arrays basics

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,39 @@
+//
+
+const arr = [0, 1, 2, 3, 4, 5]
+const myHeros =["Naruto", "Spiderman", "Ash"]
+const myArray = new Array(1,2,3,4)
+
+// console.log(myArray);
+// console.log(arr[0]);
+
+// Array Methods
+
+// myArray.push(5)
+// myArray.push(6)
+// myArray.pop()
+
+// myArray.unshift(6)
+//myArray.shift()
+
+// console.log(myArray.includes(6));
+// console.log(myArray.indexOf(3));
+
+// const  newArr = myArray.join()
+
+// console.log(myArray);
+// console.log(newArr);
+
+// Slice And Splice
+
+console.log("A", arr);
+
+const myn1 = arr.slice(1,3)
+
+console.log(myn1);
+console.log("B", arr);
+
+const myn2 = arr.splice(1,3)
+console.log(myn2);
+console.log("C", arr)
+
